7fd731cd33e6a44e3e293fae13f65cbaf01af63a
GabMalava <gabrielmalavazi_dev@outlook.com>
2025-05-13 20:19:14 -0300

    alteraçãop do task

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,11 +1,15 @@
-const { Sequelize, DataTypes } = require('sequelize');
+// src/models/Task.js
+const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Task = sequelize.define('Task', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT
+  }
 });
 
 module.exports = Task;
