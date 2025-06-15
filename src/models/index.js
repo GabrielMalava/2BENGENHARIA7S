6c7f5a41f4e9c783cfb80a57832c485ea05f1e7d
Gabriel Malavazi Rodrigues <145515242+GabrielMalava@users.noreply.github.com>
2025-06-15 16:15:40 -0300

    alterações de execuções no arquivo principal

---
+++ src/models/index.js
@@ -0,0 +1,23 @@
+const Task = require("./Task");
+const User = require("./User");
+const Board = require("./Board");
+const List = require("./List");
+
+User.hasMany(Task, { foreignKey: "userId" });
+Task.belongsTo(User, { foreignKey: "userId" });
+
+User.hasMany(Board, { foreignKey: "userId" });
+Board.belongsTo(User, { foreignKey: "userId" });
+
+Board.hasMany(List, { foreignKey: "boardId", onDelete: "CASCADE" });
+List.belongsTo(Board, { foreignKey: "boardId" });
+
+List.hasMany(Task, { foreignKey: "listId", onDelete: "CASCADE" });
+Task.belongsTo(List, { foreignKey: "listId" });
+
+module.exports = {
+  User,
+  Task,
+  Board,
+  List,
+};
