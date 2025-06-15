5e3498fe8c424c1477375c4b3dfbbdf75c1b4a80
Gabriel Malavazi Rodrigues <145515242+GabrielMalava@users.noreply.github.com>
2025-06-15 15:47:51 -0300

    Adicionada proteção de autenticação nas rotas de tarefas

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
+const auth = require('../middleware/auth');
 
-router.post('/tasks', taskController.createTask);
-router.get('/tasks', taskController.getTasks);
-router.get('/tasks/:id', taskController.getTask);
-router.put('/tasks/:id', taskController.updateTask);
-router.delete('/tasks/:id', taskController.deleteTask);
+router.post('/tasks', auth, taskController.createTask);
+router.get('/tasks', auth, taskController.getTasks);
+router.get('/tasks/:id', auth, taskController.getTask);
+router.put('/tasks/:id', auth, taskController.updateTask);
+router.delete('/tasks/:id', auth, taskController.deleteTask);
 
 module.exports = router;
